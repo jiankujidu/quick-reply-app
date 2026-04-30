9b01a99238db994d2b7177f31676c89519de8534
艰苦极度 <105758941+jiankujidu@users.noreply.github.com>
2026-04-30 09:10:30 +0800

    upload 1-3 of 150

--- build.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set JAVA_HOME=C:\AndroidSDK\openjdk\jdk-22.0.2
-set ANDROID_HOME=C:\AndroidSDK
-set PATH=%JAVA_HOME%\bin;C:\Flutter\flutter\bin;%PATH%
-cd /d C:\Users\Administrator\.qclaw\workspace\quick-reply-app
-C:\Flutter\flutter\bin\flutter.bat build apk --debug
--- quick_reply_source%2Fandroid%2Fbuild_local.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set JAVA_HOME=C:\AndroidSDK\openjdk\jdk-22.0.2
-set ANDROID_HOME=C:\AndroidSDK
-set PATH=%JAVA_HOME%\bin;%PATH%
-
-cd /d "%~dp0"
-
-"C:\ProgramData\chocolatey\lib\gradle\tools\gradle-9.4.1\bin\gradle.bat" assembleDebug --no-daemon %*
